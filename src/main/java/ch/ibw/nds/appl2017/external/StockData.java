08c3c72e4f0517e39ad2cbda73a7cf1b963d42b8
Dieter Biedermann <biedermann.dieter@powersurf.li>
2017-12-18 20:34:43 +0100

    oleaster tests eingebaut

--- src/main/java/ch/ibw/nds/appl2017/external/StockData.java
+++ src/main/java/ch/ibw/nds/appl2017/external/StockData.java
@@ -3,5 +3,6 @@ package ch.ibw.nds.appl2017.external;
 /**
  * Created by dieterbiedermann on 18.12.17.
  */
-public class StockData {
+public class  StockData {
+
 }
--- src/test/java/ch/ibw/nds/appl2017/external/StockDataTest.java
+++ src/test/java/ch/ibw/nds/appl2017/external/StockDataTest.java
@@ -1,8 +1,24 @@
-import static org.junit.Assert.*;
+package ch.ibw.nds.appl2017.external;
+
+import com.mscharhag.oleaster.runner.OleasterRunner;
+import org.junit.runner.RunWith;
+
+import static com.mscharhag.oleaster.matcher.Matchers.expect;
+import static com.mscharhag.oleaster.runner.StaticRunnerSupport.describe;
+import static com.mscharhag.oleaster.runner.StaticRunnerSupport.it;
 
 /**
  * Created by dieterbiedermann on 18.12.17.
  */
-public class StockDataTest {
+@RunWith(OleasterRunner.class)
+public class StockDataTest {{
 
-}
+    describe("Single node", () -> {
+        it("should be possible to build a single node with doctype", () -> {
+
+
+
+        });
+    });
+
+}}
