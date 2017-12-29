1b2bcbc804668afd3985d6d583ec737b9f65defc
Dieter Biedermann <biedermann.dieter@powersurf.li>
2017-12-30 00:15:11 +0100

    test fehler korrigiert

--- src/main/java/ch/ibw/nds/appl2017/controller/Correlation.java
+++ src/main/java/ch/ibw/nds/appl2017/controller/Correlation.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 public class Correlation extends ComparisonTemplate {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(AlphaVantage.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(Correlation.class);
 
     private Correlation() {
     }
@@ -28,25 +28,27 @@ public class Correlation extends ComparisonTemplate {
         super.calculate(stocks);
         List<ComparisonOutputElement> comparisonOutputElements = new ArrayList<>();
         for (int i = 0; i < stocks.size()-1; i++) {
-            double[] x = stocks.get(i)
-                    .getTimeSeries().stream()
-                    .map(TimeSerie::getClosePrice)
-                    .mapToDouble(Double::doubleValue).toArray();
-            for (int j = i+1; j < stocks.size(); j++) {
-                double[] y = stocks.get(j)
+            if (stocks.get(i).getTimeSeries() != null) {
+                double[] x = stocks.get(i)
                         .getTimeSeries().stream()
                         .map(TimeSerie::getClosePrice)
-                        .mapToDouble(Double::doubleValue)
-                        .toArray();
-                double corr = 0d;
-                if (x.length > 1 && y.length > 1) {
-                    corr = new PearsonsCorrelation().correlation(x, y);
-                    if (((Double) corr).isNaN()) {
-                        corr = 0d;
+                        .mapToDouble(Double::doubleValue).toArray();
+                for (int j = i+1; j < stocks.size(); j++) {
+                    double[] y = stocks.get(j)
+                            .getTimeSeries().stream()
+                            .map(TimeSerie::getClosePrice)
+                            .mapToDouble(Double::doubleValue)
+                            .toArray();
+                    double corr = 0d;
+                    if (x.length > 1 && y.length > 1) {
+                        corr = new PearsonsCorrelation().correlation(x, y);
+                        if (((Double) corr).isNaN()) {
+                            corr = 0d;
+                        }
                     }
+                    LOGGER.info("Symbol {} and {} have a correlation of {}", stocks.get(i).getSymbol(), stocks.get(j).getSymbol(), corr);
+                    comparisonOutputElements.add(ComparisonOutputElement.createComparisonOutputElement(stocks.get(i), stocks.get(j), corr));
                 }
-                LOGGER.info("Symbol {} and {} have a correlation of {}", stocks.get(i).getSymbol(), stocks.get(j).getSymbol(), corr);
-                comparisonOutputElements.add(ComparisonOutputElement.createComparisonOutputElement(stocks.get(i), stocks.get(j), corr));
             }
         }
         return ComparisonOutput.createComparisonOutput(comparisonOutputElements);
