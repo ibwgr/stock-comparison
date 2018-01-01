28950650cde1b75dfb00d2395a439bcffbfc6c1d
Reto Kaufmann <reto.kaufmann@adon.li>
2018-01-01 23:09:58 +0100

    SSL Problem mit anderem Endpoint (SSL Terminiert) behoben

--- src/main/java/ch/ibw/nds/appl2017/external/HttpUtils.java
+++ src/main/java/ch/ibw/nds/appl2017/external/HttpUtils.java
@@ -58,7 +58,7 @@ public class HttpUtils {
         try {
 
          // System.setProperty("javax.net.debug", "ssl");
-            System.setProperty("javax.net.ssl.trustStore", "H:\\DATEN\\IntelliJ-Play\\stock-comparison\\src\\main\\java\\ch\\ibw\\nds\\appl2017\\external\\www.alphavantage.ibw.jks");
+         // System.setProperty("javax.net.ssl.trustStore", "H:\\DATEN\\IntelliJ-Play\\stock-comparison\\src\\main\\java\\ch\\ibw\\nds\\appl2017\\external\\www.alphavantage.ibw.jks");
 
             httpResponse = HttpClientBuilder
                     .create()
