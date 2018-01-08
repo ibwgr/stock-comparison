aa55030cb31480140d50c1079af3b985589c835d
Reto Kaufmann <reto.kaufmann@adon.li>
2018-01-08 22:44:31 +0100

    docker integration test in travis (detached)

--- integrationtests.py
+++ integrationtests.py
@@ -22,5 +22,5 @@ def check_url( url, timeout=5 ):
 print "--------------------------------------------------------"
 print "Running Docker-Deployment-Test and API-Integration-Tests"
 print "--------------------------------------------------------"
-print check_url("http://127.0.0.1:8080/rest/comparison/performance?stock=SAGE&stock=GOOGL&stock=ORCL&stock=K&dateFrom=20130313&dateTo=20171231")
-print check_url("http://127.0.0.1:8080/rest/comparison/correlation?stock=SAGE&stock=GOOGL&stock=ORCL&stock=K&dateFrom=20130313&dateTo=20171231")
+print check_url("http://127.0.0.1/rest/comparison/performance?stock=SAGE&stock=GOOGL&stock=ORCL&stock=K&dateFrom=20130313&dateTo=20171231")
+print check_url("http://127.0.0.1/rest/comparison/correlation?stock=SAGE&stock=GOOGL&stock=ORCL&stock=K&dateFrom=20130313&dateTo=20171231")
