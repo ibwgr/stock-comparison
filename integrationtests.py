d787a9b478c8faa3d355c0ea2001f7f0cdf3dfc9
Reto Kaufmann <reto.kaufmann@adon.li>
2018-01-08 22:19:23 +0100

    docker integration test in travis (detached)

--- integrationtests.py
+++ integrationtests.py
@@ -2,13 +2,22 @@ import urllib2
 import socket
 
 def check_url( url, timeout=5 ):
+    print "Testing " + url
     try:
         return urllib2.urlopen(url,timeout=timeout).getcode() == 200
     except urllib2.URLError as e:
-        return False
+        print e.__doc__
+        print e.message
+        raise Exception('API Integration Test Failed (URLError)')
     except socket.timeout as e:
-        print False
+        print e.__doc__
+        print e.message
+        raise Exception('API Integration Test Failed (Socket Timeout)')
+    except Exception as e:
+        print e.__doc__
+        print e.message
+        raise Exception('API Integration Test Failed (General Exception)')
 
-
-print check_url("http://localhost:8080/rest/comparison/performance?stock=SAGE&stock=GOOGL&stock=ORCL&stock=K&dateFrom=20130313&dateTo=20171231")  #True
-print check_url("http://localhost:8080/rest/comparison/correlation?stock=SAGE&stock=GOOGL&stock=ORCL&stock=K&dateFrom=20130313&dateTo=20171231") #False
+print "Running API Integration Tests"
+print check_url("http://localhost:8080/rest/comparison/performance?stock=SAGE&stock=GOOGL&stock=ORCL&stock=K&dateFrom=20130313&dateTo=20171231")
+print check_url("http://localhost:8080/rest/comparison/correlation?stock=SAGE&stock=GOOGL&stock=ORCL&stock=K&dateFrom=20130313&dateTo=20171231")
