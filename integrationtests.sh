690590f799b80c93a663f0be9b73fe4d748f5325
Reto Kaufmann <reto.kaufmann@adon.li>
2018-01-08 23:11:01 +0100

    docker integration test in travis (detached)

--- integrationtests.sh
+++ integrationtests.sh
@@ -3,15 +3,17 @@
 set -e
 # sicherheitshalber etwas warten
 sleep 5
-echo Running Docker-Deployment-Test and API-Integration-Tests
-expected=Hello World
+echo "--------------------------------------------------------"
+echo "Running Docker-Deployment-Test and API-Integration-Tests"
+echo "--------------------------------------------------------"
+expected="Hello World"
 actual=$(curl -I http://127.0.0.1:4000/rest/comparison/performance?stock=SAGE&stock=GOOGL&stock=ORCL&stock=K&dateFrom=20130313&dateTo=20171231)
-echo Expecting: $expected
-echo Server says: $actual
+echo "Expecting:" $expected
+echo "Server says:" $actual
 if [ "$expected" != "$actual" ]; then
-  echo Test failed
+  echo "Test failed"
   exit 1
 else
-  echo Test passed
+  echo "Test passed"
   exit 0
 fi
