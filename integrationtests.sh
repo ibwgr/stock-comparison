f51540d5acdd828cf77ffbc00b92853bc699baff
Reto Kaufmann <reto.kaufmann@adon.li>
2018-01-08 23:14:44 +0100

    docker integration test in travis (detached)

--- integrationtests.sh
+++ integrationtests.sh
@@ -6,7 +6,7 @@ sleep 5
 echo "--------------------------------------------------------"
 echo "Running Docker-Deployment-Test and API-Integration-Tests"
 echo "--------------------------------------------------------"
-expected="Hello World"
+expected="Hello Worldxxx"
 actual=$(curl -I http://127.0.0.1:4000/rest/comparison/performance?stock=SAGE&stock=GOOGL&stock=ORCL&stock=K&dateFrom=20130313&dateTo=20171231)
 echo "Expecting:" $expected
 echo "Server says:" $actual
