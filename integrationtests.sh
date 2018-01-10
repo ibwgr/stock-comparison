62b67e5837d0741f55ddd33d03c2f68345e2e7d7
Reto Kaufmann <reto.kaufmann@adon.li>
2018-01-10 22:22:33 +0100

    docker deployment und end-to-end test

--- integrationtests.sh
+++ integrationtests.sh
@@ -4,11 +4,17 @@ set -e
 # sicherheitshalber etwas warten
 sleep 3
 echo "--------------------------------------------------------"
-echo "Running Docker-Deployment-Test and API-Integration-Tests"
+echo "   Running Docker-Deployment-Test and End-To-End-Tests  "
 echo "--------------------------------------------------------"
-#expected="Hello Worldxxx"
-##wget http://localhost:8080/rest/comparison/performance?stock=SAGE&stock=GOOGL&stock=ORCL&stock=K&dateFrom=20130313&dateTo=20171231 -q -O -
-curl 'http://localhost:8080/rest/comparison/performance%3Fstock%3DSAGE%26stock%3DGOOGL%26stock%3DORCL%26stock%3DK%26dateFrom%3D20130313%26dateTo%3D20171231'
+echo "calling http://localhost:8080/rest/comparison/performance?stock=SAGE&stock=GOOGL&stock=ORCL&stock=K&dateFrom=20130313&dateTo=20171231"
+curl 'http://localhost:8080/rest/comparison/performance?stock=SAGE&stock=GOOGL&stock=ORCL&stock=K&dateFrom=20130313&dateTo=20171231'
+
+expected="Hello Worldxxx"
+actual=$(curl 'http://localhost:8080/rest/comparison/performance?stock=SAGE&stock=GOOGL&stock=ORCL&stock=K&dateFrom=20130313&dateTo=20171231')
+if [[ $string == *"My long"* ]]; then
+  echo "It's there!"
+fi
+
 ##actual=$(curl -I ${service_ip}:8080/rest/comparison/performance?stock=SAGE&stock=GOOGL&stock=ORCL&stock=K&dateFrom=20130313&dateTo=20171231)
 #echo "Expecting:" $expected
 #echo "Server says:" $actual
