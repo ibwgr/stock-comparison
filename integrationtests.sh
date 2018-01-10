5c61ec7752a009b55f52df82239677922b0942ed
Reto Kaufmann <reto.kaufmann@adon.li>
2018-01-10 23:27:42 +0100

    docker deployment und end-to-end test, mit 2 tests

--- integrationtests.sh
+++ integrationtests.sh
@@ -7,11 +7,11 @@ echo "   Running Docker Deployed End-To-End-Tests  "
 echo "--------------------------------------------------------"
 echo "calling http://localhost:8080/rest/comparison/performance?stock=SAGE&stock=GOOGL&stock=ORCL&stock=K&dateFrom=20130313&dateTo=20171231"
 actualResponse1=$(curl 'http://localhost:8080/rest/comparison/performance?stock=SAGE&stock=GOOGL&stock=ORCL&stock=K&dateFrom=20130313&dateTo=20171231')
-if [[ ${actualResponse1} -eq *"comparisonOutputElements"* ]]; then
+if [[ ${actualResponse1} == *"comparisonOutputElements"* ]]; then
   echo "==> Test 1 passed"
   echo "calling http://localhost:8080/rest/comparison/correlation?stock=SAGE&stock=GOOGL&stock=ORCL&stock=K&dateFrom=20130313&dateTo=20171231"
   actualResponse2=$(curl 'http://localhost:8080/rest/comparison/correlation?stock=SAGE&stock=GOOGL&stock=ORCL&stock=K&dateFrom=20130313&dateTo=20171231')
-  if [[ ${actualResponse2} -eq *"comparisonOutputElements"* ]]; then
+  if [[ ${actualResponse2} == *"comparisonOutputElements"* ]]; then
     echo "==> Test 2 passed"
     exit 0
   else
