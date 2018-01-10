feb4cbed00752f0cba8fc1c92ad9bd670edddec0
Reto Kaufmann <reto.kaufmann@adon.li>
2018-01-10 22:59:11 +0100

    docker deployment und end-to-end test, jetzt funktioniert es!

--- integrationtests.sh
+++ integrationtests.sh
@@ -6,9 +6,16 @@ echo "--------------------------------------------------------"
 echo "   Running Docker-Deployment-Test and End-To-End-Tests  "
 echo "--------------------------------------------------------"
 echo "calling http://localhost:8080/rest/comparison/performance?stock=SAGE&stock=GOOGL&stock=ORCL&stock=K&dateFrom=20130313&dateTo=20171231"
-curl 'http://localhost:8080/rest/comparison/performance?stock=SAGE&stock=GOOGL&stock=ORCL&stock=K&dateFrom=20130313&dateTo=20171231'
-curl 'http://localhost:8080/rest/comparison/performance?stock=SAGE&stock=GOOGL&stock=ORCL&stock=K&dateFrom=20130313&dateTo=20171231'
 actual=$(curl 'http://localhost:8080/rest/comparison/performance?stock=SAGE&stock=GOOGL&stock=ORCL&stock=K&dateFrom=20130313&dateTo=20171231')
+if [[ ${actual} == *"comparisonOutputElements"* ]]; then
+  echo "==> Test passed"
+  exit 0
+else
+  echo "==> Test failed"
+  exit 1
+fi
+echo "calling http://localhost:8080/rest/comparison/correlation?stock=SAGE&stock=GOOGL&stock=ORCL&stock=K&dateFrom=20130313&dateTo=20171231"
+actual=$(curl 'http://localhost:8080/rest/comparison/correlation?stock=SAGE&stock=GOOGL&stock=ORCL&stock=K&dateFrom=20130313&dateTo=20171231')
 if [[ ${actual} == *"comparisonOutputElements"* ]]; then
   echo "==> Test passed"
   exit 0
