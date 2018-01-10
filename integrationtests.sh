65fee0dd6268557ed618e782723d6974d45f1fef
Reto Kaufmann <reto.kaufmann@adon.li>
2018-01-10 22:17:46 +0100

    docker deployment und end-to-end test

--- integrationtests.sh
+++ integrationtests.sh
@@ -8,7 +8,7 @@ echo "Running Docker-Deployment-Test and API-Integration-Tests"
 echo "--------------------------------------------------------"
 #expected="Hello Worldxxx"
 ##wget http://localhost:8080/rest/comparison/performance?stock=SAGE&stock=GOOGL&stock=ORCL&stock=K&dateFrom=20130313&dateTo=20171231 -q -O -
-curl http://localhost:8080/rest/comparison/performance%3Fstock%3DSAGE%26stock%3DGOOGL%26stock%3DORCL%26stock%3DK%26dateFrom%3D20130313%26dateTo%3D20171231
+curl 'http://localhost:8080/rest/comparison/performance%3Fstock%3DSAGE%26stock%3DGOOGL%26stock%3DORCL%26stock%3DK%26dateFrom%3D20130313%26dateTo%3D20171231'
 ##actual=$(curl -I ${service_ip}:8080/rest/comparison/performance?stock=SAGE&stock=GOOGL&stock=ORCL&stock=K&dateFrom=20130313&dateTo=20171231)
 #echo "Expecting:" $expected
 #echo "Server says:" $actual
