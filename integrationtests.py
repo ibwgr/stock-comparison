bc00aa36cf780b66d6fea41d4444629f9f2740dc
Reto Kaufmann <reto.kaufmann@adon.li>
2018-01-10 22:33:13 +0100

    docker deployment und end-to-end test

--- integrationtests.py
+++ integrationtests.py
@@ -2,7 +2,7 @@ import urllib2
 import socket
 import traceback
 
-def check_url( url, timeout=5 ):
+def check_url( url, timeout=25 ):
     print "Testing " + url
     try:
         return urllib2.urlopen(url,timeout=timeout).getcode() == 200
@@ -22,5 +22,5 @@ def check_url( url, timeout=5 ):
 print "--------------------------------------------------------"
 print "Running Docker-Deployment-Test and API-Integration-Tests"
 print "--------------------------------------------------------"
-print check_url("http://127.0.0.1/rest/comparison/performance?stock=SAGE&stock=GOOGL&stock=ORCL&stock=K&dateFrom=20130313&dateTo=20171231")
-print check_url("http://127.0.0.1/rest/comparison/correlation?stock=SAGE&stock=GOOGL&stock=ORCL&stock=K&dateFrom=20130313&dateTo=20171231")
+print check_url("http://localhost:8080/rest/comparison/performance?stock=SAGE&stock=GOOGL&stock=ORCL&stock=K&dateFrom=20130313&dateTo=20171231")
+print check_url("http://localhost:8080/rest/comparison/correlation?stock=SAGE&stock=GOOGL&stock=ORCL&stock=K&dateFrom=20130313&dateTo=20171231")
--- integrationtests.sh
+++ integrationtests.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 # Make sure tests fails if a command ends without 0
 set -e
-# sicherheitshalber etwas warten
 sleep 3
 echo "--------------------------------------------------------"
 echo "   Running Docker-Deployment-Test and End-To-End-Tests  "
@@ -11,9 +10,9 @@ curl 'http://localhost:8080/rest/comparison/performance?stock=SAGE&stock=GOOGL&s
 curl 'http://localhost:8080/rest/comparison/performance?stock=SAGE&stock=GOOGL&stock=ORCL&stock=K&dateFrom=20130313&dateTo=20171231'
 actual=$(curl 'http://localhost:8080/rest/comparison/performance?stock=SAGE&stock=GOOGL&stock=ORCL&stock=K&dateFrom=20130313&dateTo=20171231')
 if [[ ${actual} == *"comparisonOutputElements"* ]]; then
-  echo "Test passed"
-  exit 0
-else
-  echo "Test failed"
+  echo "==> Test passed"
   exit 1
+else
+  echo "==> Test failed"
+  exit 0
 fi
